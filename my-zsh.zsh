e66a21b3dc2c5a893cf90b6afa91befa99db5d54
nbari <nbari@tequila.io>
2023-05-15 17:56:26 +0200

    cleanup

--- my-zsh.zsh
+++ my-zsh.zsh
@@ -34,13 +34,38 @@ alias 6='cd -6'
 alias 7='cd -7'
 alias 8='cd -8'
 alias 9='cd -9'
-alias d='dirs -v | head -10'
+alias active='grep -Ev "^($|#)"'
+alias assh='autossh -M 0'
+alias b="upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '\$1 ~ /percentage/{print \$2}'"
+alias c='clear'
 alias connected='lsof -i | grep -E "(LISTEN|ESTABLISHED)"'
+alias cp='cp -i'
+alias cpr='rsync --delete --archive --numeric-ids --human-readable --verbose --info=progress2'
+alias d='dirs -v | head -10'
+alias dev='git checkout develop'
+alias gd="echo main diff:; git diff --name-status main develop"
+alias gl="git log --decorate --graph --oneline --all --date=short --pretty=format:'%C(bold blue)%ad%Creset %C(yellow)%h%Creset%C(auto)%d%Creset %s %C(dim magenta)<%an>%Creset %C(dim green)(%ar)%Creset'"
+alias h='history' 
+alias less='less -FSRX'
 alias listen='lsof -iTCP -sTCP:LISTEN -n -P' 
+alias main='git checkout main'
+alias master='git checkout master'
+alias mv='mv -i'
+alias myip="dig @ns1.google.com -t txt o-o.myaddr.l.google.com +short"
+alias path='echo -e ${PATH//:/\\n}'
 alias pbcopy='xclip -selection clipboard'
 alias pbpaste='xclip -selection clipboard -o'
-alias b="upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '\$1 ~ /percentage/{print \$2}'"
+alias pro='cd ~/projects'
+alias rand='LC_ALL=C; cat /dev/urandom | tr -dc a-zA-Z0-9 | fold -w 16 | head -n 1'
+alias rm='rm -i'
+alias ssh-tunnel='echo "ssh -C2qTnN -D 8080 (proxy) or -T -N -f -L 3307:db.tld:3307 host.tld"'
 alias ssh="TERM=xterm-256color ssh"
+alias t="tmux -2 attach -d || tmux -2 new"
+compdef t=tmux
+alias tl='tmux list-sessions'
+alias tmp='cd ~/tmp'
+alias up='git add . && git commit -a -m "sync `date`" && git push'
+alias up='git add . && git commit -am "sync $(date)" && git push'
 
 if type exa 2>&1 >/dev/null; then
   alias ls='exa'
@@ -62,6 +87,37 @@ bindkey -v
 bindkey "^?" backward-delete-char
 
 # ----------------------------------------------------------------------------
+# csh history
+# ----------------------------------------------------------------------------
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
+bindkey -M vicmd 'k' up-line-or-beginning-search # Up
+bindkey -M vicmd 'j' down-line-or-beginning-search # Down
+
+# ----------------------------------------------------------------------------
+# use OS time
+# ----------------------------------------------------------------------------
+# disable -r time
+
+# ----------------------------------------------------------------------------
+# magic-space
+# ----------------------------------------------------------------------------
+bindkey " " magic-space # do history expansion on space !XX<space>
+
+# ----------------------------------------------------------------------------
+# Edit command using vim - hit ESC and then v
+# emacs mode:  Ctrl-x e -  bindkey '^Xe' edit-command-line
+# ----------------------------------------------------------------------------
+export KEYTIMEOUT=1
+autoload edit-command-line
+zle -N edit-command-line
+bindkey -M vicmd v edit-command-line
+
+# ----------------------------------------------------------------------------
 # Advanced Tab-completion
 # ----------------------------------------------------------------------------
 autoload -U compinit && compinit
@@ -81,6 +137,7 @@ setopt pushdminus
 setopt pushdsilent
 setopt pushdtohome
 setopt pushd_ignore_dups
+unsetopt nomatch
 
 # ----------------------------------------------------------------------------
 # Command history configuration
@@ -181,3 +238,111 @@ function slick_prompt_precmd() {
 function slick_prompt_preexec() {
     slick_prompt_timestamp=$EPOCHSECONDS
 }
+
+
+# ----------------------------------------------------------------------------
+# custom
+# ----------------------------------------------------------------------------
+
+# get PID/PGID/PPID/SID to certain process or pid:
+pgid() {
+    ps -ejf | egrep "STIME | $1" | grep -v egrep
+}
+
+get_headers_GET() {
+    curl -i -L -s -H "Accept-Encoding: gzip,deflate" -A "nbari - [$(date -u '+%FT%T')]" -D - $1 -o /dev/null
+}
+
+get_headers() {
+    curl -I -L -H "Accept-Encoding: gzip,deflate" -H "Origin: http://example.com" -H "Access-Control-Request-Method: GET" $1
+}
+
+get_options() {
+    curl -I -L -X OPTIONS -H "Origin: http://example.com" -H "Access-Control-Request-Method: GET" -H "Access-Control-Request-Headers: X-Requested-With" $1
+}
+
+get_akamai() {
+    curl -I -L -H "Pragma: akamai-x-cache-on, akamai-x-cache-remote-on, akamai-x-check-cacheable, akamai-x-get-cache-key, akamai-x-get-extracted-values, akamai-x-get-nonces, akamai-x-get-ssl-client-session-id, akamai-x-get-true-cache-key, akamai-x-serial-no, akamai-x-get-request-id, akamai-x-request-trace, akamai-x--meta-trace, akama-xi-get-extracted-values" $1
+}
+
+set_env() {
+    if [ -r $PWD/.zsh_config ]; then
+        source $PWD/.zsh_config
+        print -P -- %F{2}Ok%f
+    else
+        print -P -- %F{9}No .zsh_config found%f
+    fi
+}
+
+curl_time() {
+    curl -o /dev/null -Ls -w " \
+        time_namelookup:  %{time_namelookup}\n \
+        time_connect:  %{time_connect}\n \
+        time_appconnect:  %{time_appconnect}\n \
+        time_pretransfer:  %{time_pretransfer}\n \
+        time_redirect:  %{time_redirect}\n \
+        time_starttransfer:  %{time_starttransfer}\n \
+        ----------\n \
+        time_total:  %{time_total}\n" "$1"
+}
+
+# ----------------------------------------------------------------------------
+# Kill all process that match $1
+# ----------------------------------------------------------------------------
+kill9() {
+    for pid in `ps aux | grep -v "grep" | grep "$@" | awk '{print $2}'`; do
+        kill -9 $pid && echo "Killed ${pid}"
+    done
+}
+
+# ----------------------------------------------------------------------------
+# ssh+tmux
+# ----------------------------------------------------------------------------
+export AUTOSSH_GATETIME=0
+export AUTOSSH_POLL=10
+export AUTOSSH_PORT=0
+s() {
+    [[ ! -z $1 ]] && autossh -M 0 -t $@ tmux -2 new -ADs $USER
+}
+compdef s=ssh
+
+m() {
+    [[ ! -z $1 ]] && mosh $@ -- tmux -2 new -ADs $USER
+}
+
+# multiple tmux windows
+ms() {
+    [[ ! -z $1 ]] && tmux new-window; while read line; do (tmux split-window -h "autossh -M0 $line $2"; tmux select-layout tiled); done< <(awk '{print $NF}' <(host $1); tmux kill-pane -t0)
+}
+
+# ----------------------------------------------------------------------------
+# tmux
+# ----------------------------------------------------------------------------
+if hash tmux &> /dev/null; then
+    if [ -z "$TMUX" ]; then
+        tmux -2 new
+    elif [[ $(who am i) =~ '\([-a-zA-Z0-9\.]+\)$' ]] || [ ! -z "$SSH_CONNECTION" ] || [ ! -z "$REMOTEHOST" ]; then
+        tmux set-option -g status-right '#[fg=colour003][ #H - #[fg=colour111]#(uname) #[fg=colour003]]#[fg=colour231]#(uptime | grep -o "...user.*")' > /dev/null
+        tmux set-option -g status-position bottom > /dev/null
+        tmux set-option -g window-status-current-bg colour071 > /dev/null
+        tmux has-session || tmux -2 new
+    fi
+fi
+
+eval "$(direnv hook zsh)"
+
+# --files: List files that would be searched but do not search
+# --no-ignore: Do not respect .gitignore, etc...
+# --hidden: Search hidden files and folders
+# --follow: Follow symlinks
+# --glob: Additional conditions for search (in this case ignore everything in the .git/ folder)
+export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
+export FZF_TMUX=1
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+# command history
+fh() {
+    print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
+}
+
+
